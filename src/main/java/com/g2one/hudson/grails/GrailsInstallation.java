0438d0b77fe9b402060e2377696dabc31f3222f9
kiy0taka <kiy0taka333+github@gmail.com>
2011-08-20 21:14:12 +0900

    Implement EnvironmentSpecific, NodeSpecific.

--- src/main/java/com/g2one/hudson/grails/GrailsInstallation.java
+++ src/main/java/com/g2one/hudson/grails/GrailsInstallation.java
@@ -2,14 +2,20 @@ package com.g2one.hudson.grails;
 
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import hudson.EnvVars;
+import hudson.model.EnvironmentSpecific;
+import hudson.model.TaskListener;
+import hudson.model.Node;
+import hudson.slaves.NodeSpecific;
 import hudson.tools.ToolProperty;
 import hudson.tools.ToolInstallation;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 
-public final class GrailsInstallation extends ToolInstallation {
+public final class GrailsInstallation extends ToolInstallation implements EnvironmentSpecific<GrailsInstallation>, NodeSpecific<GrailsInstallation> {
 
     public GrailsInstallation(String name, String home) {
         super(name, home);
@@ -36,5 +42,13 @@ public final class GrailsInstallation extends ToolInstallation {
 
     public boolean getExists() {
         return getExecutable().exists();
+    }
+
+    public GrailsInstallation forNode(Node node, TaskListener log) throws IOException, InterruptedException {
+        return new GrailsInstallation(getName(), translateFor(node, log), getProperties().toList());
+    }
+
+    public GrailsInstallation forEnvironment(EnvVars environment) {
+        return new GrailsInstallation(getName(), environment.expand(getHome()), getProperties().toList());
     }
 }
