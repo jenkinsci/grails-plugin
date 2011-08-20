67d64a5b5e04101705fa2846d7094b15d46b388f
kiy0taka <kiy0taka333+github@gmail.com>
2011-08-20 21:23:07 +0900

    Added GrailsInstaller.

---
+++ src/main/java/com/g2one/hudson/grails/GrailsInstaller.java
@@ -0,0 +1,27 @@
+package com.g2one.hudson.grails;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import hudson.tools.DownloadFromUrlInstaller;
+import hudson.tools.ToolInstallation;
+
+public class GrailsInstaller extends DownloadFromUrlInstaller {
+
+    @DataBoundConstructor
+    public GrailsInstaller(String id) {
+        super(id);
+    }
+
+    public static class DescriptorImpl extends DownloadFromUrlInstaller.DescriptorImpl<GrailsInstaller> {
+
+        @Override
+        public String getDisplayName() {
+            return "Install from mirrors";
+        }
+
+        @Override
+        public boolean isApplicable(Class<? extends ToolInstallation> toolType) {
+            return toolType == GrailsInstallation.class;
+        }
+    }
+}
